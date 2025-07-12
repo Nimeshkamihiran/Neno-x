b696d44b8f512a8d1ec270df97112ba517751e5f
Nimeshkamihiran <nimeshmihiranga183@gmail.com>
2025-07-12 20:00:28 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'NENON X Bot',
+  author: '‎',
+  botName: "NENON X Bot",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '94721584279', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;
